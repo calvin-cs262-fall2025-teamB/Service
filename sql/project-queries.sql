da8b03443bd858cbb06548df6bf7adca8504c5ce
MrCatMan49 <159658897+MrCatMan49@users.noreply.github.com>
2025-10-20 13:38:27 -0400

    added completedAdventure table, updated types, added more queries

--- sql/project-queries.sql
+++ sql/project-queries.sql
@@ -1,36 +1,52 @@
--- Get the number of Game records.
-SELECT *
-  FROM Player
-  ;
-SELECT *
-  FROM Region
-  ;
-SELECT *
-  FROM Landmark
-  ;
-SELECT *
-  FROM Adventure
-  ;
-SELECT *
-  FROM Token
-  ;
+-- SELECT *
+--   FROM Player
+--   ;
+-- SELECT *
+--   FROM Region
+--   ;
+-- SELECT *
+--   FROM Landmark
+--   ;
+-- SELECT *
+--   FROM Adventure
+--   ;
+-- SELECT *
+--   FROM Token
+--   ;
 
 -- Get the player records.
 SELECT * 
-  FROM User
+  FROM Player
   ;
 
 -- Get all landmarks in Region 1.
 SELECT *
-    FROM Landmark
-    WHERE regionID = 1
-;
+  FROM Landmark
+  WHERE regionID = 1
+  ;
 
 -- Get all tokens in Adventure 1.
 SELECT *
-    FROM Token
-    WHERE adventureID = 1
-;
+  FROM Token
+  WHERE adventureID = 1
+  ;
+
+-- Get all adventures made by player w/ ID 1
+SELECT *
+  FROM Adventure
+  WHERE playerID = 1
+  ;
+
+-- Get all completed adventures by player w/ ID 1
+SELECT *
+  FROM CompletedAdventure
+  WHERE playerID = 1
+  ;
+
+-- Get the cross-product of all the tables.
+SELECT *
+  FROM Player, Region, Landmark, Adventure, Token
+  ;
 
 -- -- Get the highest score ever recorded.
 --   SELECT score
@@ -38,8 +54,3 @@ SELECT *
 -- ORDER BY score DESC
 --    LIMIT 1
 --    ;
-
--- Get the cross-product of all the tables.
-SELECT *
-  FROM Player, Region, Landmark, Adventure, Token
-  ;
--- sql/project.sql
+++ sql/project.sql
@@ -14,14 +14,14 @@ CREATE TABLE Region (
     ID SERIAL PRIMARY KEY,
     name varchar(50) NOT NULL,
     playerID integer REFERENCES Player(ID),
-    location integer ARRAY[2]
+    location point
 );
 
 CREATE TABLE Landmark (
     ID SERIAL PRIMARY KEY,
     name varchar(50) NOT NULL,
     regionID integer REFERENCES Region(ID),
-    location integer ARRAY[2]
+    location point
 );
 
 CREATE TABLE Adventure (
@@ -29,44 +29,46 @@ CREATE TABLE Adventure (
     name varchar(50) NOT NULL,
     playerID integer REFERENCES Player(ID),
     regionID integer REFERENCES Region(ID),
-    location integer ARRAY[2]
+    location point
 );
 
 CREATE TABLE Token (
     ID SERIAL PRIMARY KEY,
     adventureID integer REFERENCES Adventure(ID),
-    location integer ARRAY[2]
+    location point
 );
 
--- CREATE TABLE CompletedAdventure(
---     ID SERIAL PRIMARY KEY,
---     userID REFERENCES User(ID),
---     adventureID REFERENCES Adventure(ID),
---     completionDate date,
---     completionTime timestamp,
--- );
+CREATE TABLE CompletedAdventure (
+    playerID integer REFERENCES Player(ID),
+    adventureID integer REFERENCES Adventure(ID),
+    completionDate date,
+    completionTime interval
+);
 
 GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON Region TO PUBLIC;
 GRANT SELECT ON Landmark TO PUBLIC;
 GRANT SELECT ON Adventure TO PUBLIC;
 GRANT SELECT ON Token TO PUBLIC;
--- GRANT SELECT ON CompletedAdventure TO PUBLIC;
+GRANT SELECT ON CompletedAdventure TO PUBLIC;
 
 -- Add sample records
 INSERT INTO Player(name) VALUES ('Jacob');
 INSERT INTO Player(name) VALUES ('Billy Bob Joe');
 
-INSERT INTO Region(name, playerID, location) VALUES ('Calvin University', 1, ARRAY[0, 0]);
-INSERT INTO Region(name, playerID, location) VALUES ('Hope', 2, ARRAY[1, 1]);
+INSERT INTO Region(name, playerID, location) VALUES ('Calvin University', 1, POINT(0, 0));
+INSERT INTO Region(name, playerID, location) VALUES ('Hope', 2, POINT(1, 1));
 
-INSERT INTO Landmark(name, regionID, location) VALUES ('Cheeze Statue', 1, ARRAY[0, 0]);
-INSERT INTO Landmark(name, regionID, location) VALUES ('SB Vending Machine', 1, ARRAY[0, 0]);
-INSERT INTO Landmark(name, regionID, location) VALUES ('Dining Hall', 2, ARRAY[1, 1]);
+INSERT INTO Landmark(name, regionID, location) VALUES ('Cheeze Statue', 1, POINT(0, 0));
+INSERT INTO Landmark(name, regionID, location) VALUES ('SB Vending Machine', 1, POINT(0, 0));
+INSERT INTO Landmark(name, regionID, location) VALUES ('Dining Hall', 2, POINT(1, 1));
 
-INSERT INTO Adventure(name, regionID, location) VALUES (1, 1, ARRAY[0, 0]);
-INSERT INTO Adventure(name, regionID, location) VALUES (2, 2, ARRAY[1, 1]);
+INSERT INTO Adventure(name, playerID, regionID, location) VALUES (1, 1, 1, POINT(0, 0));
+INSERT INTO Adventure(name, playerID, regionID, location) VALUES (2, 2, 2, POINT(1, 1));
 
-INSERT INTO Token(adventureID, location) VALUES (1, ARRAY[0, 0]);
-INSERT INTO Token(adventureID, location) VALUES (1, ARRAY[10, 10]);
-INSERT INTO Token(adventureID, location) VALUES (2, ARRAY[2, 2]);
+INSERT INTO Token(adventureID, location) VALUES (1, POINT(0, 0));
+INSERT INTO Token(adventureID, location) VALUES (1, POINT(10, 10));
+INSERT INTO Token(adventureID, location) VALUES (2, POINT(2, 2));
+
+INSERT INTO CompletedAdventure(playerID, adventureID, completionDate, completionTime)
+    VALUES (1, 1, '1000-01-13', '1:50:30');
