7213e1f041a1e85294181865f182cb7961b182f3
MrCatMan49 <159658897+MrCatMan49@users.noreply.github.com>
2025-10-31 15:12:35 -0400

    more thourough schema, and started queries

--- sql/project-queries.sql
+++ sql/project-queries.sql
@@ -1,4 +1,56 @@
--- Get the player records.
+---- Playing Adventure Queries ----
+-- Get Count of Tokens in Adventure X
+
+-- Get Landmarks in Region X
+
+-- Get tokens ordered by tokenOrder
+
+
+---- Homepage ----
+-- Get all regions made by Adventurer
+
+-- Get all adventures sorted by distance
+
+-- Get all adventures in Region X
+
+---- Adventure Page ----
+-- Get adventure X
+
+---- Profile Page ----
+-- Get Adventurer data X
+SELECT *
+  FROM Adventurer
+  WHERE ID = X
+  ;
+-- Get count of all completed adventures by adventurer X
+SELECT COUNT(*)
+  FROM CompletedAdventure
+  WHERE adventurerID = X
+  ;
+-- Get count of all tokens collected by player
+SELECT COUNT(*)
+  FROM Token CompletedAdventure
+  WHERE CompletedAdventure.adventurerID = X
+  AND Token.adventureID = CompletedAdventure.ID
+  ;
+-- Get count of all adventures created by player
+SELECT COUNT(*)
+  FROM Adventure
+  WHERE adventurerID = X
+  ;
+-- Get all adventures completed by player sorted by completion date
+SELECT *
+  FROM CompletedAdventure
+  WHERE playerID = 1
+  ORDER BY completionDate DESC
+  ;
+
+---- Creation ----
+
+
+
+
+-- Get the adventurer records.
 SELECT * 
   FROM Player
   ;
@@ -21,29 +73,16 @@ SELECT *
   WHERE playerID = 1
   ;
 
--- Get all completed adventures by player w/ ID 1
-SELECT *
-  FROM CompletedAdventure
-  WHERE playerID = 1
-  ;
-
 -- Get all completed adventures by player w/ ID 1, sorted by most recent
 SELECT *
   FROM CompletedAdventure
   WHERE playerID = 1
   ORDER BY completionDate DESC
   ;
+
 -- Get all completed adventures by player w/ ID 1, sorted by completion time
 SELECT *
   FROM CompletedAdventure
   WHERE playerID = 1
   ORDER BY completionTime
   ;
-
-
-
-
--- -- Get the cross-product of all the tables.
--- SELECT *
---   FROM Player, Region, Landmark, Adventure, Token
---   ;
--- sql/project.sql
+++ sql/project.sql
@@ -7,22 +7,32 @@ DROP TABLE IF EXISTS Adventurer;
 
 CREATE TABLE Adventurer (
     ID SERIAL PRIMARY KEY,
-    name varchar(50) NOT NULL
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    profilePicture VARCHAR(255)  -- stores something like "/images/user123.jpg"
+
 );
 
 CREATE TABLE Region (
+    -- IDs
     ID SERIAL PRIMARY KEY,
-
-    name varchar(50) NOT NULL,
     adventurerID integer REFERENCES Adventurer(ID),
-    location point
+
+    -- Descriptive data
+    name TEXT NOT NULL,
+    description TEXT,
+    
+    -- Region geospacial information
+    location point NOT NULL,
+    radius integer NOT NULL
+
 );
 
 CREATE TABLE Landmark (
     ID SERIAL PRIMARY KEY,
     regionID integer REFERENCES Region(ID),
 
-    name varchar(50) NOT NULL,
+    name TEXT NOT NULL,
     location point
 );
 
@@ -31,7 +41,8 @@ CREATE TABLE Adventure (
     adventurerID integer REFERENCES Adventurer(ID),
     regionID integer REFERENCES Region(ID),
 
-    name varchar(50) NOT NULL,
+    name TEXT NOT NULL,
+    numTokens integer,
     location point
 );
 
@@ -39,7 +50,9 @@ CREATE TABLE Token (
     ID SERIAL PRIMARY KEY,
     adventureID integer REFERENCES Adventure(ID),
 
-    location point
+    location point,
+    hint TEXT,
+    tokenOrder integer
 );
 
 CREATE TABLE CompletedAdventure (
@@ -47,9 +60,26 @@ CREATE TABLE CompletedAdventure (
     adventurerID integer REFERENCES Adventurer(ID),
     adventureID integer REFERENCES Adventure(ID),
 
+    -- Player Data
     completionDate date,
     completionTime interval
 );
+
+-- CREATE TABLE AdventureInProgress (
+--     ID SERIAL PRIMARY KEY,
+--     adventurerID integer REFERENCES Adventurer(ID),
+--     adventureID integer REFERENCES Adventure(ID),
+
+--     dateStarted date,
+-- );
+
+-- CREATE TABLE AdventureInProgressToken (
+--     ID SERIAL PRIMARY KEY,
+--     adventureID integer REFERENCES Adventure(ID),
+--     tokenID integer REFERENCES Token(ID),
+
+--     isCollected boolean,
+-- );
 
 GRANT SELECT ON Adventurer TO PUBLIC;
 GRANT SELECT ON Region TO PUBLIC;
@@ -59,38 +89,36 @@ GRANT SELECT ON Token TO PUBLIC;
 GRANT SELECT ON CompletedAdventure TO PUBLIC;
 
 -- Add sample records
-INSERT INTO Adventurer(name) VALUES ('Jacob');
-INSERT INTO Adventurer(name) VALUES ('Kevin');
-INSERT INTO Adventurer(name) VALUES ('Billy Bob Joe');
+INSERT INTO Adventurer(username, password, profilePicture) VALUES ('Adventurer 1', '123', 'pictures/img1.jpg');
+INSERT INTO Adventurer(username, password, profilePicture) VALUES ('Adventurer 2', '456', 'pictures/img2.jpg');
+INSERT INTO Adventurer(username, password, profilePicture) VALUES ('Adventurer 3', '789', 'pictures/img3.jpg');
 
-INSERT INTO Region(name, adventurerID, location) VALUES ('Calvin University', 1, POINT(0, 0));
-INSERT INTO Region(name, adventurerID, location) VALUES ('Hope', 2, POINT(1, 1));
+INSERT INTO Region(name, adventurerID, description, location, radius) VALUES ('Region 1', 1, "Region 1 Desc", POINT(0, 0), 100);
+INSERT INTO Region(name, adventurerID, description, location, radius) VALUES ('Region 2', 2, "Region 2 Desc", POINT(0, 0), 100);
+INSERT INTO Region(name, adventurerID, description, location, radius) VALUES ('Region 3', 3, "Region 3 Desc", POINT(0, 0), 100);
 
-INSERT INTO Landmark(name, regionID, location) VALUES ('Cheeze Statue', 1, POINT(0, 0));
-INSERT INTO Landmark(name, regionID, location) VALUES ('SB Vending Machine', 1, POINT(0, 0));
-INSERT INTO Landmark(name, regionID, location) VALUES ('Bunker Center', 1, POINT(0, 0));
-INSERT INTO Landmark(name, regionID, location) VALUES ('Dining Hall', 2, POINT(1, 1));
-INSERT INTO Landmark(name, regionID, location) VALUES ('Dorms', 2, POINT(1, 1));
+INSERT INTO Landmark(name, regionID, location) VALUES ('Landmark 1', 1, POINT(0, 0));
+INSERT INTO Landmark(name, regionID, location) VALUES ('Landmark 2', 1, POINT(0, 0));
+INSERT INTO Landmark(name, regionID, location) VALUES ('Landmark 3', 2, POINT(0, 0));
+INSERT INTO Landmark(name, regionID, location) VALUES ('Landmark 4', 2, POINT(0, 0));
+INSERT INTO Landmark(name, regionID, location) VALUES ('Landmark 5', 3, POINT(0, 0));
+INSERT INTO Landmark(name, regionID, location) VALUES ('Landmark 6', 3, POINT(0, 0));
 
-INSERT INTO Adventure(name, adventurerID, regionID, location) VALUES ('Adventure 1', 1, 1, POINT(0, 0));
-INSERT INTO Adventure(name, adventurerID, regionID, location) VALUES ('Adventure 2', 2, 1, POINT(0, 0));
-INSERT INTO Adventure(name, adventurerID, regionID, location) VALUES ('Adventure 3', 3, 1, POINT(0, 0));
-INSERT INTO Adventure(name, adventurerID, regionID, location) VALUES ('Adventure 4', 1, 2, POINT(1, 1));
-INSERT INTO Adventure(name, adventurerID, regionID, location) VALUES ('Adventure 5', 2, 2, POINT(1, 1));
-INSERT INTO Adventure(name, adventurerID, regionID, location) VALUES ('Adventure 5', 3, 2, POINT(1, 1));
+INSERT INTO Adventure(name, adventurerID, regionID, location, numTokens) VALUES ('Adventure 1', 1, 1, POINT(0, 0), 1);
+INSERT INTO Adventure(name, adventurerID, regionID, location, numTokens) VALUES ('Adventure 2', 2, 1, POINT(0, 0), 2);
+INSERT INTO Adventure(name, adventurerID, regionID, location, numTokens) VALUES ('Adventure 3', 3, 1, POINT(0, 0), 3);
 
 -- Adventure 1 --
-INSERT INTO Token(adventureID, location) VALUES (1, POINT(0, 0));
-INSERT INTO Token(adventureID, location) VALUES (1, POINT(10, 10));
+INSERT INTO Token(adventureID, location, hint, tokenOrder) VALUES (1, POINT(0, 0), "Adv 1: Tok 1: Hint", 1);
+INSERT INTO Token(adventureID, location, hint, tokenOrder) VALUES (1, POINT(10, 10), "Adv 1: Tok 2: Hint", 2);
 
 -- Adventure 2 --
-INSERT INTO Token(adventureID, location) VALUES (2, POINT(2, 2));
-INSERT INTO Token(adventureID, location) VALUES (2, POINT(20, 20));
+INSERT INTO Token(adventureID, location, hint, tokenOrder) VALUES (2, POINT(0, 0), "Adv 2: Tok 1: Hint", 1);
+INSERT INTO Token(adventureID, location, hint, tokenOrder) VALUES (2, POINT(10, 10), "Adv 2: Tok 2: Hint", 2);
 
 -- Adventure 3 --
-INSERT INTO Token(adventureID, location) VALUES (3, POINT(3, 3));
-INSERT INTO Token(adventureID, location) VALUES (3, POINT(30, 30));
-
+INSERT INTO Token(adventureID, location, hint, tokenOrder) VALUES (3, POINT(0, 0), "Adv 3: Tok 1: Hint", 1);
+INSERT INTO Token(adventureID, location, hint, tokenOrder) VALUES (4, POINT(10, 10), "Adv 3: Tok 2: Hint", 2);
 
 INSERT INTO CompletedAdventure(adventurerID, adventureID, completionDate, completionTime)
     VALUES (1, 2, '1000-01-13', '4:50:30');
